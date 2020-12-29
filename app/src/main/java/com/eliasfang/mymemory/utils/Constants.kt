3def2c43d9705d937433f155155907c6522ada95
Elias Fang <eliasfang@gmail.com>
2020-12-28 20:09:14 -0800

    Add Custom Game to Firestore

--- app/src/main/java/com/eliasfang/mymemory/utils/Constants.kt
+++ app/src/main/java/com/eliasfang/mymemory/utils/Constants.kt
@@ -3,6 +3,7 @@ package com.eliasfang.mymemory.utils
 import com.eliasfang.mymemory.R
 
 const val EXTRA_BOARD_SIZE = "EXTRA_BOARD_SIZE"
+const val EXTRA_GAME_NAME = "EXTRA_GAME_NAME"
 val DEFAULT_ICONS = listOf(
         R.drawable.ic_face,
         R.drawable.ic_flower,
